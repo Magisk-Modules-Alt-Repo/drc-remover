aad6796b6b08fb9a73256b36da695b0eb936dd4d
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-03-14 20:24:16 +0900

    Add files via upload
    typos

--- functions3.sh
+++ functions3.sh
@@ -5,16 +5,16 @@
 
 function no_need_this_module()
 {
-  ui_print "*********************************************************"
+  ui_print "*****************************************************************"
   ui_print " This module isn't needed because this device hasn't been DRC enabled! "
-  ui_print "*********************************************************"
+  ui_print "*****************************************************************"
 }
 
 function policy_file_not_found()
 {
-  ui_print "************************************"
+  ui_print "********************************************"
   ui_print " This module cannot find any audio policy file! "
-  ui_print "************************************"
+  ui_print "********************************************"
 }
 
 # Get the active audio policy configuration fille from the audioserever
@@ -28,10 +28,10 @@ function getActivePolicyFile()
 
 function stopDRC()
 {
-    # stopDRC has two args specifying a main audio policy configuration XML file (eg. audio_policy_configuration.xml) and its dummy one to be overridden
+    # stopDRC has two args specifying a main audio policy configuration XML file (eg. audio_policy_configuration.xml) and its modified one to overlay
 
      if [ $# -eq 2  -a  -r "$1"  -a  -w "$2" ]; then
-        # Copy and override an original audio_policy_configuration.xml to its dummy file
+        # Copy an original audio_policy_configuration.xml
         cp -f "$1" "$2"
         # Change audio_policy_configuration.xml file to remove DRC
         sed -i 's/speaker_drc_enabled[:space:]*=[:space:]*"true"/speaker_drc_enabled="false"/' "$2"
